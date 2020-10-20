e185428538a7b1c61fc6ecabb7737db5e4521700
Anderson Ma <30582996+Ma-Weijian@users.noreply.github.com>
2020-10-20 14:53:41 +0800

    Add files via upload

---
+++ example_programs/correct.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+# define FOO(A, B, ..., format, ...) \
+if(X == Y) { \
+    fprintf(stderr, format __W32API_MAJOR_VERSION(,) __ARGS__); \
+}
+
+char str[30] = "This is a string literal.\n";
+
+// Main Function
+int main(int argc, char *argv[])
+{
+    /* This 
+     * is a comment. */
+    unsigned long long a = 114514Ull;
+    double b = .24e+10f;
+    2222;
+    0x3d.11p+9;
+    int a = 3;
+    double c = 0xab.cdp+18f;
+    __uint128_t c = ++a;
+    a >> 4;
+    return 0;
+}
+
---
+++ example_programs/faulty.c
@@ -0,0 +1,15 @@
+#include <no/such/header.h>
+
+int main(bool argc, char argv, float foo)
+{
+    int @@caz;
+    double xsw$2;
+    xsw2 = 3355.866vgt;
+    int 1abb;
+    2223
+    `D `DE;
+    double d = 0xabcdesf.achp+ef;
+    return -1;
+}
+
+
